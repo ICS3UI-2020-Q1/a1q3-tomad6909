9f555590b642a47818b9a2e1df58dc94d7b56751
tomad6909 <tomad6909@wrdsb.ca>
2020-09-14 16:03:58 +0000

    Finished A1Q3

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program prompts the user how many of each item they want to purchase
+ * @author David Toma
  */
 public class Main {
 
@@ -9,7 +10,36 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a scanner for the user
+    Scanner input = new Scanner(System.in);
+
+    // ask user for how many chromebook chargers they want
+    System.out.println("How many chromebook chargers do you want?");
+
+    int chromebook = input.nextInt();
+
+    System.out.println("How many motherboards do you want?");
+
+    int motherboard = input.nextInt();
+
+    System.out.println("How many mice do you want?");
+
+    int mice = input.nextInt();
+
+    double totalChromebook = chromebook * 34.99;
+    double totalMotherboard = motherboard * 127.50;
+    double totalMice = mice * 18.00;
+
+    double subtotal = totalChromebook + totalMotherboard + totalMice;
+
+    System.out.println("The subtotal is $" + subtotal);
+
+    double tax = subtotal * 0.13;
+
+    System.out.println("The tax is $" + tax);
+
+    double total = subtotal + tax;
+
+    System.out.println("The total is $" + total);
   }
 }
